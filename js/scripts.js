013b5e312283d9ec9e85579d76f00991f1109277
Charline Koch <charline.koch2@gmail.com>
2018-05-29 14:56:41 +0200

    Ajout des menus gauche et droit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+// LEFT MENU
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+// RIGHT MENU
+
+/* Set the width of the side navigation to 250px */
+function openSearch() {
+    document.getElementById("mySearchnav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeSearch() {
+    document.getElementById("mySearchnav").style.width = "0";
+}
