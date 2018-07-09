cd98c44bf42a8549444ac9c47feee756cfe69e21
Benjamin Soufflet <contact@benjaminsoufflet.fr>
2018-07-09 11:45:34 +0200

    Modif default controller, Delete event

--- src/ArticleBundle/Controller/DefaultController.php
+++ src/ArticleBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('ArticleBundle:Default:index.html.twig');
+        return $this->render('@Article/Default/index.html.twig');
     }
 }
